5b0a1b63bbba933ce321c59e164c4b6f4f4890c8
Rushi-H <Rushihonkande@gmail.com>
2025-11-15 10:31:04 +0530

    aaa

---
+++ 2.js
@@ -0,0 +1,14 @@
+function addTwoNumbers(num1, num2) {
+    let sum = num1 + num2;
+    let sum1 = "Hello World111";
+     console.log(sum1);
+    // return num1 + num2;
+    return sum;
+} 
+
+console.log(addTwoNumbers(3, 5))
+
+const result = addTwoNumbers(10, 20);
+console.log(result);
+// console.log(sum1);
+
---
+++ 3.js
@@ -0,0 +1,10 @@
+
+
+function userLoginMassge(username = 'mahesh') {
+    if(!username) {
+        return `hello plz inter your name`;}
+        else{ 
+    return `Hlo, ${username}! Welcome back to our platform.`;}
+}
+
+console.log(userLoginMassge('ruhi'));
